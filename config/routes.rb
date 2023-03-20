edbc93eb6ac72d20811ae4d5297b4b9219f6558a
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-03-20 13:38:44 +0000

    [Fix]adminのURLの調整

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
     passwords:     'public/passwords',
     registrations: 'public/registrations'
   }
-  devise_for :admins, controllers: {
-    sessions:      'admins/sessions',
-    passwords:     'admins/passwords',
-    registrations: 'admins/registrations'
+  devise_for :admin, controllers: {
+    sessions:      'admin/sessions',
+    passwords:     'admin/passwords',
+    registrations: 'admin/registrations'
   }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
