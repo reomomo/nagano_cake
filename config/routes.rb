38208c08f33c20883ec1f24eafda58730e2c4c93
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-03-21 00:15:21 +0000

    ルーティングの簡素化

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :customers, controllers: {
-    sessions:      'public/sessions',
-    passwords:     'public/passwords',
-    registrations: 'public/registrations'
-  }
-  devise_for :admin, controllers: {
-    sessions:      'admin/sessions',
-    passwords:     'admin/passwords',
-    registrations: 'admin/registrations'
-  }
+  devise_for :customers, module: "public"
+  devise_for :admin, module: "admin"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
