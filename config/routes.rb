77fee8bee937a114a2171fcad3df6e428fc11751
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-03-21 01:48:28 +0000

    コントローラとビュー（index、edit）の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
   devise_for :customers, module: "public"
   devise_for :admin, module: "admin"
 
