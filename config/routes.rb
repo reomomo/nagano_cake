465dd03396476b2c1a72781361e8458dfb0449f6
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-03-21 08:03:42 +0000

    [Update]render先の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+
   devise_for :customers, module: "public"
   devise_for :admin, module: "admin"
 
+  root to: 'public/homes#top'
+  get '/about' => 'public/homes#about'
+
+  scope module: :public do
+    resources :items, only: [:index, :show]
+  end
+
+  namespace :admin do
+    get '/' => 'homes#top'
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :customers, only: [:show, :index, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
