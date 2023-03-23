87316fc5ee80a6942e02b7d60a0086b166ea02d6
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-03-23 04:06:07 +0000

    [Update]ビューの詳細

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,22 @@
 class Admin::CustomersController < ApplicationController
   def show
+    @customer = Customer.find(params.id)
   end
 
   def index
+    @customers = Customer.all
   end
 
   def edit
+    @customer = Customer.find(params.id)
+    @customer.update
+    rdirect_t admin_customer_path(@customer.id)
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
+  end
+
 end
