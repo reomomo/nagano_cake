dd5597252e788f606ccd92d1bdb3a5e30340b925
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-03-24 10:25:43 +0000

    [Add]enumの設定

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,6 @@
 class Admin::HomesController < ApplicationController
   def top
     @orders = Order.all
+    @order_detail = OrderDetail.all
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
-  enum payment_methods:{credit_card:0,bank_transfer:1}
+  enum payment_method: { credit_card: 0, transfer: 1 }
 #credit_card=クレジットカード、bank_transfer＝銀行振込
-  enum status:{payment_waiting:0,payment_confirmation:1,in_production:2,preparing_delivery:3,delivered:4}
+  enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
 # payment_waiting=入金待ち、payment_confirmation=入金確認、in_production=製作中、
 # preparing_delivery=発送準備中、delivered=発送済み
 
