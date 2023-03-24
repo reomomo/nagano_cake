4881d00120ad683e29790ccbd966c3b46e2a99ac
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-03-24 12:55:02 +0000

    [Update]注文履歴画面追加

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,5 @@
 class OrderDetail < ApplicationRecord
-  enum status: {production_not_possible: 0, production_pending: 1, in_production: 2, production_complete: 3 }
+  enum makingstatus: {production_not_possible: 0, production_pending: 1, in_production: 2, production_complete: 3 }
 # production_not_possible=製作不可、production_pending=制作待ち、in_production=製作中、production_complete=製作完了
 
 belongs_to :order
