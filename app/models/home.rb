be6560f10945c029472c27dbc3911df0606961c1
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-03-29 09:25:19 +0000

    [Update}top画面作成

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,9 +1,13 @@
 class Home < ApplicationRecord
   has_one_attached :profile_image
 
+  # def top
+  #   @items = Item.all.order(created_at: :desc)
+  # end
+
   def get_image(width, height)
   　 unless image.attached?
-  　　file_path = Rails.root.join('app/assets/images/waku_batu.png')
+  　　file_path = Rails.root.join('app/assets/images/wakubatsu.jpg')
   　　image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   　end
   　image.variant(resize_to_limit: [width, height]).processed
