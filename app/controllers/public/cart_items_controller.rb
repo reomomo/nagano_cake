137704039983d640c9bd2134e7a3c65498fedd82
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-03-30 10:48:48 +0000

    [Update]カート画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,4 +3,15 @@ class Public::CartItemsController < ApplicationController
     @cart_items = CartItem.all
     # Book.all.sum(@sum_price)
   end
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+
+private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+  end
+
 end
