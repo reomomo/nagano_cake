5f051e720bc6245a9f64485609518b9a5db0c42a
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-04-03 10:11:27 +0000

    [Update]退会処理確認中

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,7 +3,7 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   before_action :customer_state, only: [:create]
-  
+
   def after_sign_in_path_for(resource)
     root_path
   end
@@ -33,11 +33,14 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   protected
-  
+
   def customer_state
     @customer = Customer.find_by(email: params[:customer][:email])
     return if !@customer
     if @customer.valid_password?(params[:customer][:password])
+      if @customeris_deleted == true
+        redirect_to new_customer_registration_path
+      end
     end
   end
 end
