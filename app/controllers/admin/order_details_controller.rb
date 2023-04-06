4527ad81e2070ebbc74ac2976efad5636ef5897e
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-04-06 01:11:34 +0000

    [Update]ステータスが連動して変更されるように修正中

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,13 +4,22 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order = Order.find(@order_detail.order.id)
-    @order_detail.update(order_detail_params)
+    @order_details = @order.order_details
+    if @order.status == 1
+      @order_details.update(making_status: 1)
+    else
+      @order_detail.update(order_detail_params)
+    end
     redirect_to admin_order_path(@order.id)
   end
 
   private
   def order_detail_params
     params.require(:order_detail).permit(:item_id, :order_id, :product_sum, :amount, :making_status)
+  end
+
+  def order_params
+    params.require(:order).permit(:customer_id, :delivery_target_postal_code, :delivery_address, :delivery_target_full_name, :postage, :pay_amount, :payment_methods, :status)
   end
 
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,7 +9,13 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update(order_params)
+    @order_details = @order.order_details
+    if @order.status == 1
+      @order.update(order_params)
+      @order_details.update(making_status: 1)
+    else
+      @order.update(order_params)
+    end
     redirect_to admin_order_path(@order.id)
   end
 
@@ -17,5 +23,9 @@ class Admin::OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:customer_id, :delivery_target_postal_code, :delivery_address, :delivery_target_full_name, :postage, :pay_amount, :payment_methods, :status)
   end
+
+  # def order_detail_params
+  #   params.require(:order_detail).permit(:item_id, :order_id, :product_sum, :amount, :making_status)
+  # end
 
 end
